42326f3eaf3044efd5118e30a6c36a26c4c1e320
akboy <ak47@swehack.org>
2016-10-03 17:43:17 +0200

    Update portscanner.py

--- portscanner.py
+++ portscanner.py
@@ -4,28 +4,15 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server = '192.168.1.1'
 #Insert an URL or an IP Adress to scan above...
-
-
 def pscan(port):
     try:
         s.connect((server,port))
         return True
     except:
         return False
-
 #Type port range to scan...
 for x in range(1,81):
     if pscan(x):
         print('Port',x,'is open!!!    *****')
     else:
         print('Port',x,'is closed')
-
-
-
-
-
-
-#Super easy port scanner by Ak47, don't laugh...
-# :D
-
-

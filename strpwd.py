7f16e3309b07e49845306d9cf43a5b5a63459f9e
akboy <ak47swe@protonmail.com>
2016-11-16 20:25:30 +0100

    Update strpwd.py

--- strpwd.py
+++ strpwd.py
@@ -2,4 +2,5 @@ import string
 from random import *
 characters = string.ascii_letters+string.punctuation+string.digits
 password = "".join(choice(characters) for x in range(randint(13, 23)))
+print('This is your generated password')
 print(password)
